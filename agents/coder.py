0c86a32a26e0ea58df84a76b47e345f14d53c3cd
Saravanan G <159242456+Saravanan-20G@users.noreply.github.com>
2026-04-13 17:00:22 +0530

    Update coder.py

--- agents/coder.py
+++ agents/coder.py
@@ -1,10 +1,14 @@
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
+# from langchain_openai import ChatOpenAI
+# from dotenv import load_dotenv
+# import os
+
+# load_dotenv()
+
+# api_key = os.getenv("GROQ_API_KEY")
 import os
+import streamlit as st
 
-load_dotenv()
-
-api_key = os.getenv("GROQ_API_KEY")
+api_key = os.getenv("GROQ_API_KEY") or st.secrets["GROQ_API_KEY"]
 
 llm = ChatOpenAI(
     base_url="https://api.groq.com/openai/v1",
